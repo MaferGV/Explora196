761e3ea07a27de2af480d1a2c277fa29fce9be35
MaferGV <mafergalic@gmail.com>
2024-06-07 08:18:51 -0600

    Add files via upload

---
+++ efectos.js
@@ -0,0 +1,12 @@
+function not(){
+var sound=new Audio();
+sound.src="avi.mp3";
+sound.play();
+}
+function link(){
+setTimeout(function en(){
+location.href='index.html';}
+,5250);
+
+}
+<!--window.location.-->
